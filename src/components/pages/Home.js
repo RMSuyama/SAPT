5dc362d8d88bca98fe032520bf8d106382c8911d
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-06-21 21:09:13 -0300

    qwe

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from '../views/Navbar';
 import Citacao from '../views/citacao';
 import Countdown from '../views/countdown';
-import Mailme from '../services/mailme';
 
 const Home = () => {
   return (
